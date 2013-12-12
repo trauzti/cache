6d2abc124d9e5a06acb51f82b7ab2959459e7c07
Trausti Sæmundsson <trauzti@gmail.com>
2013-12-12 16:03:14 +0000

    get_stackdistance in LRU, more readable code and a test!

--- LRU.py
+++ LRU.py
@@ -1,74 +1,127 @@
+import sys
+
+
 class Node:
-    def __init__(self, key):
+    def __init__(self, key, val=None):
         self.key = key
         self.next = None
         self.prev = None
+        self.val = val     # needed for evictions
 
     def __repr__(self):
-        return "Node(%s)" % self.key
-
+        return "Node(%s -> %s)" % (self.key, self.val)
 
 class alg:
-    def walk(self):
-        n = self.first
-        while n:
-            print n.key
-            if n.next:
-                assert n.next.prev == n
-            if n.prev:
-                assert n.prev.next == n
-            n = n.next
-
-    def __repr__(self):
-        return "LRU"
-
     def __init__(self, c):
         # c is cache size
         self.c = c
         self.cn = 0
-        self.first = None
-        self.last = None
+        self.head = None
+        self.tail = None
         self.stored = {}
         self.hitcount = 0
         self.count = 0
+
+    # Walks the list and asserts that links are correct
+    def walk(self):
+        n = self.head
+        s = "head"
+
+        while n:
+            s+= "->(%s)" % n.key
+            if n.prev:
+                assert n.prev.next == n
+            if n.next:
+                assert n.next.prev == n
+            else:
+                break
+            n = n.next
+        s += "<- tail"
+        assert n == self.tail
+        print s
+
+    # returns (dist, found)
+    # if found == true then the stackdistance of key is in dist and
+    # 0 <= dist < c
+    # if found == false, the key wasnt found in the cache
+    def get_stackdistance(self, key):
+        n = self.head
+        dist = 0
+        found = False
+        while n:
+            if key == n.key:
+                found = True
+                break
+            n = n.next
+            dist += 1
+        return (dist, found)
+
+    def __repr__(self):
+        return "LRU"
 
     def setup(self, reqlist):
         # I'm an online algorithm :-)
         pass
 
+    def unlink(self, n):
+        if n == self.head:
+            self.head = n.next
+        if n == self.tail:
+            #print "updating tail in unlink"
+            self.tail = n.prev
+            pass
+        if n.prev:
+            n.prev.next = n.next
+        if n.next:
+            n.next.prev = n.prev
+
+
+    # put n at the LRU head
+    # maintains tail and head correctly
+    def update_head(self, n):
+        # Update the LRU head
+        n.prev = None
+        n.next = self.head
+
+        if self.head:
+            self.head.prev = n
+        self.head = n
+
+    # usage x = get(key)
+    # if the key is found in the cache, put it in the LRU head and return the value
+    # else return None
     def get(self, key):
         self.count += 1
         if key in self.stored:
             self.hitcount += 1
             n = self.stored[key]
-            if n != self.first:
-                if n == self.last:
-                    self.last = n.prev
-                else:
-                    n.next.prev = n.prev # Unneccessary if last
-                n.prev.next = n.next     # Unneccessary if first
-                n.prev = None            #
-                self.first.prev = n      # Put it in front
-                n.next = self.first      # ...
-                self.first = n           # Make it first
+            if n != self.head:
+                self.unlink(n)
+                self.update_head(n)
+            if not self.tail:
+                self.tail = n
 
+            return n.val
+        return None
 
-        else:
-            self.put(key)
-
-    def put(self, key):
+    def put(self, key, val=None):
         if key not in self.stored:
-            n = Node(key)
+            n = Node(key, val)
             self.stored[key] = n
-            n.next = self.first
-            if self.first:
-                self.first.prev = n
-            self.first = n
-            if self.cn == self.c:
-                del self.stored[self.last.key]
-                self.last = self.last.prev
-                self.last.next = None
-            else:
-                self.cn += 1
-                if not self.last:
-                    self.last = n
+        else:
+            # Overwrite it correctly
+            n = self.stored[key]
+            n.val = val
+            if n != self.head:
+                self.unlink(n)
+                self.update_head(n)
+            if not self.tail:
+                self.tail = n
+
+        if self.cn == self.c:
+            # Evict the tail
+            del self.stored[self.tail.key]
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            self.cn += 1
---
+++ test_LRU.py
@@ -0,0 +1,22 @@
+from LRU import alg
+
+
+lru = alg(5)
+
+lru.walk()
+print "Loop starting"
+for i in xrange(5):
+    lru.put(i, i*i)
+    assert lru.get(i) == i*i
+    assert lru.head.key == i
+    lru.walk()
+    dist, found  =  lru.get_stackdistance(0)
+    assert dist == i
+assert lru.tail.key == 0
+
+assert lru.get(0) == 0
+lru.walk()
+assert lru.tail.key == 1
+
+dist, found  =  lru.get_stackdistance(0)
+assert dist == 0
